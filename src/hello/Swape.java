64b3554c5d49eb70869718aeecea8abda5dd5097
sadique <mdsadiqueamin786@gmail.com>
2022-09-19 21:17:09 +0530

    Third Commit

---
+++ src/hello/Swape.java
@@ -0,0 +1,16 @@
+package Exp;
+
+public class Swape {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		b=b+a;
+		a=b-a;
+		b=b-a;
+		System.out.println("a = "+a+" b ="+b);
+		
+		
+	}
+
+}
